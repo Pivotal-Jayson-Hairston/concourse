f77510c09e2ba63e8802f795d439f1ee790d3390
Josh Zarrabi <jzarrabi@pivotal.io>
2016-09-13 13:47:15 -0700

    remove line breaks from batch file
    Signed-off-by: Topher Bullock <cbullock@pivotal.io@pivotal.io>

--- ci/scripts/fly-build.bat
+++ ci/scripts/fly-build.bat
@@ -7,8 +7,8 @@ set /p FINAL_VERSION=<%CD%\final-version\version
 set GOARCH=amd64
 set CGO_ENABLED=0
 
+echo %FINAL_VERSION%
+
 cd .\concourse\src\github.com\concourse\fly
 
-go build -ldflags "-X github.com/concourse/fly/version.Version=%FINAL_VERSION%" \
-  -o "%platform%/fly.exe" \
-  github.com/concourse/fly
+go build -ldflags "-X github.com/concourse/fly/version.Version=%FINAL_VERSION%" -o "%platform%/fly.exe" github.com/concourse/fly
