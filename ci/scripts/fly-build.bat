15f76897fccff549ab35cfbe6f6c7d4b4f3b7cd9
Josh Zarrabi <jzarrabi@pivotal.io>
2016-09-13 16:47:42 -0700

    fixed fly build scripts
    Signed-off-by: Topher Bullock <cbullock@pivotal.io@pivotal.io>

--- ci/scripts/fly-build.bat
+++ ci/scripts/fly-build.bat
@@ -5,4 +5,4 @@ set PATH=%CD%\concourse\bin;%PATH%
 
 set /p FinalVersion=<final-version\version
 
-go build -ldflags "-X github.com/concourse/fly/version.Version=%FinalVersion%" -o windows/fly.exe github.com/concourse/fly
+go build -ldflags "-X github.com/concourse/fly/version.Version=%FinalVersion%" -o windows/fly_windows_amd64.exe github.com/concourse/fly
