9ad0cdb91c7cf41fa33bdcdc94a4967d64b00dca
Josh Zarrabi <jzarrabi@pivotal.io>
2016-09-13 14:00:15 -0700

    change version set command
    Signed-off-by: Topher Bullock <cbullock@pivotal.io@pivotal.io>

--- ci/scripts/fly-build.bat
+++ ci/scripts/fly-build.bat
@@ -2,13 +2,7 @@ set PATH=C:\Go\bin;%PATH
 
 set GOPATH=%CD%\concourse
 set PATH=%CD%\concourse\bin;%PATH%
-set /p FINAL_VERSION=<%CD%\final-version\version
 
-set GOARCH=amd64
-set CGO_ENABLED=0
+set /p FinalVersion=<final-version\version
 
-echo %FINAL_VERSION%
-
-cd .\concourse\src\github.com\concourse\fly
-
-go build -ldflags "-X github.com/concourse/fly/version.Version=%FINAL_VERSION%" -o "%platform%/fly.exe" github.com/concourse/fly
+go build -ldflags "-X github.com/concourse/fly/version.Version=%FinalVersion%" -o windows/fly.exe github.com/concourse/fly
